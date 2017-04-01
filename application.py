71b27f632cc4ed96bc392bd799896429b623c699
Eero Lehtonen <eero.o.lehtonen@aalto.fi>
2017-04-01 15:08:24 +0300

    app -> application

--- application.py
+++ application.py
@@ -3,19 +3,19 @@ from flask import render_template
 from flask.ext.babel import Babel
 from flask import request
 
-app = Flask(__name__)
-app.config.from_object('config.BaseConfig')
-babel = Babel(app)
+application = Flask(__name__)
+application.config.from_object('config.BaseConfig')
+babel = Babel(application)
 
 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(['fi', 'en'])
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-  app.debug = True
-  app.run()
+  application.debug = True
+  application.run()
