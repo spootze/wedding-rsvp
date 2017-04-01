1be59b9341587dd303401368d77a7988fb8247e7
Olli Niskanen <olli.niskanen@gmail.com>
2017-04-01 15:00:43 +0300

    Added flask-babel for translations

--- application.py
+++ application.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask.ext.babel import Babel
+from flask import request
+
 app = Flask(__name__)
+app.config.from_object('config.BaseConfig')
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(['fi', 'en'])
 
 @app.route('/')
-def hello_world():
+def index():
+    print app.config['BABEL_DEFAULT_LOCALE']
+    # return "Hello"
     return render_template('index.html')
---
+++ config.py
@@ -0,0 +1,5 @@
+
+class BaseConfig(object):
+    SUPPORTED_LANGUAGES = {'fi': 'Finnish', 'en': 'English'}
+    BABEL_DEFAULT_LOCALE = 'fi'
+    BABEL_DEFAULT_TIMEZONE = 'UTC+2'
