c17b29b9d847aa681bda089b2693ffc01508ef79
Olli Niskanen <olli.niskanen@gmail.com>
2017-04-01 16:32:13 +0300

    Updated header translations. Added map embed demo

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
 
 class BaseConfig(object):
     SUPPORTED_LANGUAGES = {'fi': 'Finnish', 'en': 'English'}
     BABEL_DEFAULT_LOCALE = 'fi'
     BABEL_DEFAULT_TIMEZONE = 'UTC+2'
+    GOOGLE_MAPS_API_KEY = os.getenv('GOOGLE_MAPS_API_KEY')
